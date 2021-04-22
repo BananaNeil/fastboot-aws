98e99ed1a04f59490f7554a5e164dbeb8ea6130b
Neil Johnson <BananaNeil@gmail.com>
2021-04-22 23:20:34 +0700

    Added an IMAGE_HOST option to the fastboot server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const S3Downloader      = require('fastboot-s3-downloader');
 const S3Notifier        = require('fastboot-s3-notifier');
 const RedisCache        = require('fastboot-redis-cache');
 const FastBootAppServer = require('fastboot-app-server');
+const ExpressHTTPServer = require('fastboot-app-server/src/express-http-server');
+
 
 const S3_BUCKET    = process.env.FASTBOOT_S3_BUCKET;
 const S3_KEY       = process.env.FASTBOOT_S3_KEY;
@@ -12,6 +14,7 @@ const REDIS_PORT   = process.env.FASTBOOT_REDIS_PORT;
 const REDIS_EXPIRY = process.env.FASTBOOT_REDIS_EXPIRY;
 const USERNAME     = process.env.FASTBOOT_USERNAME;
 const PASSWORD     = process.env.FASTBOOT_PASSWORD;
+const IMAGE_HOST   = process.env.FASTBOOT_IMAGE_HOST;
 
 let downloader = new S3Downloader({
   bucket: S3_BUCKET,
@@ -34,8 +37,17 @@ if (REDIS_HOST || REDIS_PORT) {
   console.log('No FASTBOOT_REDIS_HOST or FASTBOOT_REDIS_PORT provided; caching is disabled.');
 }
 
+const httpServer = new ExpressHTTPServer(/* {options} */);
+if (IMAGE_HOST) {
+  const app = httpServer.app;
+  app.get(/^((?!\/assets\/).)*\.(png)|(jpg)|(gif)|(jpeg)|(pdf)$/, function (req, res) {
+    res.redirect(IMAGE_HOST + '/'+ req.path);
+  });
+}
+
 let server = new FastBootAppServer({
   downloader: downloader,
+  httpServer: httpServer,
   notifier: notifier,
   username: USERNAME,
   password: PASSWORD,
